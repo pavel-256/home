52f38a809d57f9c2c5ad8f1bc07cc6b590cad715
pavel-256 <pavel256@windowslive.com>
2020-08-28 13:23:42 +0300

    Updates

---
+++ precache-manifest.77060982a1711013e401d523ec182f0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb534281fd4cf6f2791c2c10bb50e56",
+    "url": "/smartHome/index.html"
+  },
+  {
+    "revision": "425994146f63518bfe46",
+    "url": "/smartHome/static/css/main.ad26df27.chunk.css"
+  },
+  {
+    "revision": "be835f15d78868502c6e",
+    "url": "/smartHome/static/js/2.83510648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smartHome/static/js/2.83510648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425994146f63518bfe46",
+    "url": "/smartHome/static/js/main.19c03605.chunk.js"
+  },
+  {
+    "revision": "b64379404731190077b0",
+    "url": "/smartHome/static/js/runtime-main.13be4919.js"
+  }
+]);
