659112c5056caadae4a921d95afec59305975348
pavel-256 <paveldev320@gmail.com>
2021-02-17 17:24:40 +0200

    Updates

---
+++ precache-manifest.74880b5c9b1828f0d671278ac221d011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a09cd0a4e6132db044f25ff3e837090",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a9c40c105da503ec2955",
+    "url": "/home/static/css/main.2c0e8499.chunk.css"
+  },
+  {
+    "revision": "d8fd31bba6679cca8423",
+    "url": "/home/static/js/2.c1f31525.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/home/static/js/2.c1f31525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c40c105da503ec2955",
+    "url": "/home/static/js/main.fd4cae3c.chunk.js"
+  },
+  {
+    "revision": "327519257ae83559732d",
+    "url": "/home/static/js/runtime-main.1ebb12cf.js"
+  }
+]);
