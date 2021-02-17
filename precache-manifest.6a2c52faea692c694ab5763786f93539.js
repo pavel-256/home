ec321d96ef954f25559a855d57ca70e15e20aa12
pavel-256 <paveldev320@gmail.com>
2021-02-17 17:10:34 +0200

    Updates

---
+++ precache-manifest.6a2c52faea692c694ab5763786f93539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f54388d4296ea5263aaf1415b252ed",
+    "url": "/smartHome/index.html"
+  },
+  {
+    "revision": "25bb07142097b546e471",
+    "url": "/smartHome/static/css/main.2c0e8499.chunk.css"
+  },
+  {
+    "revision": "31f86af5a9342e43a367",
+    "url": "/smartHome/static/js/2.91cce777.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/smartHome/static/js/2.91cce777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bb07142097b546e471",
+    "url": "/smartHome/static/js/main.4782b236.chunk.js"
+  },
+  {
+    "revision": "eafb0ba5da482918cba3",
+    "url": "/smartHome/static/js/runtime-main.0284ad3a.js"
+  }
+]);
