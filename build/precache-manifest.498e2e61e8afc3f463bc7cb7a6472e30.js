f80977f20f4f47161978bd208575eefe0ca76114
pavel-256 <paveldev320@gmail.com>
2021-02-17 17:52:12 +0200

    update

---
+++ build/precache-manifest.498e2e61e8afc3f463bc7cb7a6472e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644524cb81d3abf7209c804e3d253779",
+    "url": "/smartHome/index.html"
+  },
+  {
+    "revision": "cdce20b9d2eed91275fd",
+    "url": "/smartHome/static/css/main.2c0e8499.chunk.css"
+  },
+  {
+    "revision": "d8fd31bba6679cca8423",
+    "url": "/smartHome/static/js/2.c1f31525.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/smartHome/static/js/2.c1f31525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdce20b9d2eed91275fd",
+    "url": "/smartHome/static/js/main.f99f1dc9.chunk.js"
+  },
+  {
+    "revision": "b64379404731190077b0",
+    "url": "/smartHome/static/js/runtime-main.13be4919.js"
+  }
+]);
